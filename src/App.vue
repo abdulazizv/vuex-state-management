f46d9e88d3987d71ef2b88e3f01df80073896b68
abdulazizv <abdulazizheir@gmail.com>
2023-02-27 10:02:46 +0500

    some functions configured

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,43 @@
 <template>
-  <div>
-    <RouterView />
+  <header class="bg-cyan-700">
+    <div class="container mx-auto">
+    <nav class="bg-cyan-700 h-[75px] flex justify-between px-4 items-center">
+      <a href="#" class="text-white text-2xl">VUE-ROUTER</a>
+      <ul class="flex w-3/12 justify-between">
+        <li><RouterLink to="/">Home</RouterLink></li>
+        <li><RouterLink to="/post">Post</RouterLink></li>
+        <li><RouterLink to="/user">Users</RouterLink></li>
+      </ul>
+    </nav>
   </div>
+  </header>
+  <main>
+    <section class="main">
+      <div class="container mx-auto px-5">
+          <RouterView />
+      </div>
+    </section>
+  </main>
+  <footer class="bg-cyan-800 text-white p-5 text-center">
+    <div class="container mx-auto">
+      VUE-ROUTER
+    </div>
+  </footer>
 </template>
 <script>
-import { RouterView } from 'vue-router';
+import { RouterView,RouterLink } from 'vue-router';
 export default {
   name:'App'
 }
 </script>
 <style lang="css">
-  
+  .router-link-active {
+    font-weight: bold;
+    text-decoration: underline;
+    color:white;
+    font-size: larger;
+  }
+  .main {
+    min-height: calc(100vh - 75px - 75px);
+  }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,24 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from '../views/Home.vue';
-
+import Post from '../views/Post.vue';
+import User from '../views/User.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes:[
     {
-      paht:'/',
+      path:'/',
       name:'home',
       component:Home
+    },
+    {
+      path:'/post',
+      name:'post',
+      component:Post
+    },
+    {
+      path:'/user',
+      name:'user',
+      component:User
     }
   ]
 })
