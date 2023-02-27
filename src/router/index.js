047f71f05548cebc768466dc2f2f2b01824cf1ec
abdulazizv <abdulazizheir@gmail.com>
2023-02-27 11:03:45 +0500

    elements being dynamic

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from '../views/Home.vue';
 import Post from '../views/Post.vue';
 import User from '../views/User.vue';
-import About from '../views/About.vue'
+import About from '../views/About.vue';
+import PostItem from '../views/PostItem.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes:[
@@ -15,6 +16,11 @@ const router = createRouter({
       path:'/post',
       name:'post',
       component:Post
+    },
+    {
+      path:'/post/:id',
+      name:'postitem',
+      component:PostItem
     },
     {
       path:'/user',
---
+++ src/views/PostItem.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="p-5 w-3/4 shadow m-4 border">
+        <h1 class="px-5">POST ITEM 1</h1>
+    </div>
+</template>
+<script>
+export default {
+    name:'postitem',
+    
+}
+</script>
+<style lang="css">
+    
+</style>
