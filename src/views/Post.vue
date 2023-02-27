acebe8c8029310da4a9ef8df6842097f2db86bbb
abdulazizv <abdulazizheir@gmail.com>
2023-02-27 11:47:37 +0500

    added new methods

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,76 +1,85 @@
 <template>
-    <div>
-        <h1 class="text-center m-5 font-bold uppercase text-2xl">Post</h1>
+  <div>
+    <h1 class="text-center m-5 font-bold uppercase text-2xl">Post</h1>
 
-        <ul class="shadow-lg w-2/3 mx-auto p-5 border" v-if="isLoading">
-            <li v-for="(item) in post" class="shadow">{{ item.id }} {{ item.title }}</li>
-        </ul>
-        <span class="loader text-center items-center  ml-[600px] mt-12" v-else="isLoading"></span>
-    </div>
+    <ul class="shadow-lg w-2/3 mx-auto p-5 border" v-if="isLoading">
+      <li v-for="item in post" class="shadow">
+        <RouterLink
+          class="shadow p-4 my-1 border block"
+          :to="`/post/${item.id}`"
+          >{{ item.id }} {{ item.title }}</RouterLink
+        >
+      </li>
+    </ul>
+    <span
+      class="loader text-center items-center ml-[600px] mt-12"
+      v-else="isLoading"
+    ></span>
+  </div>
 </template>
 <script>
-import axios from '../service/axios'
+import axios from "../service/axios";
+import { RouterLink } from "vue-router";
 export default {
-    name:'post',
-    data() {
-        return {
-            post:[],
-            isLoading:false,
+  name: "post",
+  data() {
+    return {
+      post: [],
+      isLoading: false,
+    };
+  },
+  methods: {
+    async getPost() {
+      try {
+        const response = await axios.get("/posts");
+        if (response.status == 200) {
+          console.log(response.data);
+          this.isLoading = true;
+          this.post = response.data;
         }
+      } catch (error) {
+        console.log(error);
+      }
     },
-    methods:{
-         async getPost() {
-            try {
-                const response = await axios.get('/posts');
-                if(response.status == 200) {
-                    setTimeout(() => {
-                        this.isLoading = true;  
-                    },4000)    
-                    this.post = response.data
-                }   
-            } catch (error) {
-                console.log(error)
-            }
-        }
-    },
-    mounted() {
-        this.getPost()
-    }
-}
+  },
+  mounted() {
+    this.getPost();
+  },
+};
 </script>
 <style lang="css">
-    .loader {
-        width: 84px;
-        height: 84px;
-        position: relative;
-      }
-      .loader:before , .loader:after {
-        content: "";
-        position: absolute;
-        right: 0;
-        top: 0;
-        width:84px;
-        height: 84px;
-        border-radius: 50%;
-        background:#FF3D00;
-        animation: push 1s infinite linear alternate;
-      }
-      .loader:after {
-        top: auto;
-        bottom: 0;
-        left: 0;
-        background: #fff;
-        animation-direction: alternate-reverse;
-      }
-      @keyframes push {
-          0% {
-            width:14px;
-            height: 14px;
-          }
-          100% {
-            width:84px;
-            height: 84px;
-          }
-      }
-  
+.loader {
+  width: 84px;
+  height: 84px;
+  position: relative;
+}
+.loader:before,
+.loader:after {
+  content: "";
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 84px;
+  height: 84px;
+  border-radius: 50%;
+  background: #ff3d00;
+  animation: push 1s infinite linear alternate;
+}
+.loader:after {
+  top: auto;
+  bottom: 0;
+  left: 0;
+  background: #fff;
+  animation-direction: alternate-reverse;
+}
+@keyframes push {
+  0% {
+    width: 14px;
+    height: 14px;
+  }
+  100% {
+    width: 84px;
+    height: 84px;
+  }
+}
 </style>
--- src/views/PostItem.vue
+++ src/views/PostItem.vue
@@ -1,12 +1,53 @@
 <template>
+    <div class="w-full border p-5 shadow-lg my-5">
+        <button @click="$route.params.go(-1)" class="bg-cyan-500 px-3 py-2 rounded-lg text-white font-bold focus:ring-4 focus:ring-cyan-600">GO BACK</button>            
+        </div>
     <div class="p-5 w-3/4 shadow m-4 border">
-        <h1 class="px-5">POST ITEM 1</h1>
+        
+        <div v-if="isLoad">
+            <h1 class="px-5"> {{ $route.params.id  }} {{ post.title }}</h1>
+        <p class="px-5 my-3">
+            {{ post.body }}
+        </p>
+    </div>
+    <div v-else="isLoad">
+        <h1>LOADING>>>....</h1>
+    </div>
     </div>
 </template>
 <script>
+import axios from '../service/axios'
 export default {
     name:'postitem',
-    
+
+    data() {
+        return {
+            post:[],
+            isLoad :false
+        }
+    },
+    methods:{
+        getId() {
+            console.log(this.$route.params.id);
+        },
+        async getPostItem() {
+            try {
+                const postItem = await axios.get(`posts/${this.$route.params.id}`);
+                if(postItem.status == 200) {
+                    console.log(postItem.data)
+                    this.post = postItem.data;
+                    this.isLoad = true
+                }
+            } catch (error) {
+                console.log(error)
+            }
+        },
+        async goBack()
+    },
+    mounted() {
+        this.getId(),
+        this.getPostItem()
+    }
 }
 </script>
 <style lang="css">
